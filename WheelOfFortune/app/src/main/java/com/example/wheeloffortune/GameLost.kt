eac0534a0dac847aa344603a66e58d9d7354a91d
Rasm-P <rasmus.pra@gmail.com>
2021-11-22 16:43:14 +0100

    Added game won and game lost pages, as well as play again button

--- WheelOfFortune/app/src/main/java/com/example/wheeloffortune/GameLost.kt
+++ WheelOfFortune/app/src/main/java/com/example/wheeloffortune/GameLost.kt
@@ -1,28 +1,33 @@
 package com.example.wheeloffortune
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import androidx.navigation.findNavController
+import com.example.wheeloffortune.databinding.FragmentGameLostBinding
+import com.example.wheeloffortune.databinding.FragmentWordGuessingBinding
 import kotlin.properties.Delegates
 
 class GameLost : Fragment() {
-    private var points by Delegates.notNull<Int>()
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_lost, container, false)
+        var view = inflater.inflate(R.layout.fragment_game_lost, container, false)
+        var points = arguments?.let { it.getInt("points") }
+        view.findViewById<TextView>(R.id.game_points).text = points.toString()
+        view.findViewById<TextView>(R.id.play_again_button).setOnClickListener { playAgain() }
+
+        return view
     }
 
-    //Needs work
-    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
-        super.onViewCreated(view, savedInstanceState)
-        arguments?.let { points = it.getInt("points") }
-
-        view.findViewById<TextView>(R.id.game_points).setText(points)
+    private fun playAgain() {
+        view?.findNavController()?.navigate(R.id.action_gameLost_to_wordGuessing)
     }
 }
--- WheelOfFortune/app/src/main/java/com/example/wheeloffortune/GameWon.kt
+++ WheelOfFortune/app/src/main/java/com/example/wheeloffortune/GameWon.kt
@@ -5,6 +5,8 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.TextView
+import androidx.navigation.findNavController
 
 class GameWon : Fragment() {
 
@@ -12,6 +14,15 @@ class GameWon : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.fragment_game_won, container, false)
+        var view = inflater.inflate(R.layout.fragment_game_won, container, false)
+        var points = arguments?.let { it.getInt("points") }
+        view.findViewById<TextView>(R.id.game_points).text = points.toString()
+        view.findViewById<TextView>(R.id.play_again_button).setOnClickListener { playAgain() }
+
+        return view
+    }
+
+    private fun playAgain() {
+        view?.findNavController()?.navigate(R.id.action_gameWon_to_wordGuessing)
     }
 }
